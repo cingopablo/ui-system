3c218d97aef2b570c96bf8ac1f14a10710558993
Pablo Cingolani <cingopablo@gmail.com>
2022-08-22 11:26:58 +0200

    Updates Codebox

--- src/components/Codebox/Codebox.styles.tsx
+++ src/components/Codebox/Codebox.styles.tsx
@@ -1,15 +1,14 @@
 import { styled } from '../../theming/stitches.config'
 
 export const CodeboxWrapper = styled('div', {
+  '@media (max-width: 600px)': {
+    display: 'block',
+    overflow: 'hidden',
+  },
   '.sp-layout': {
     position: 'relative',
     marginBottom: '2.25rem',
     borderRadius: 'var(--border-radius-2)',
-
-    '@media (max-width: 600px)': {
-      display: 'block',
-      overflow: 'hidden',
-    },
   },
   '.cm-gutterElement': {
     fontSize: '12px',
