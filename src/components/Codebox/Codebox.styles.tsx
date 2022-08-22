dacdbee4d113cb5b70706bf75f5051f9b3980817
Pablo Cingolani <cingopablo@gmail.com>
2022-08-22 11:19:21 +0200

    Updates Codebox

--- src/components/Codebox/Codebox.styles.tsx
+++ src/components/Codebox/Codebox.styles.tsx
@@ -5,6 +5,10 @@ export const CodeboxWrapper = styled('div', {
     position: 'relative',
     marginBottom: '2.25rem',
     borderRadius: 'var(--border-radius-2)',
+
+    '@media (max-width: 600px)': {
+      display: 'block',
+    },
   },
   '.cm-gutterElement': {
     fontSize: '12px',
