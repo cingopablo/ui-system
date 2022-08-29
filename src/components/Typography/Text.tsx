a9c46efe06acf83e34a7337acba7e40328d5735b
Pablo Cingolani <cingopablo@gmail.com>
2022-08-29 11:43:32 +0200

    Updates

--- src/components/Typography/Text.tsx
+++ src/components/Typography/Text.tsx
@@ -1,7 +1,7 @@
 import { styled } from '../../theming/stitches.config'
 import { text } from '../../theming/tokens'
 
-export const Text = styled('span', {
+export const Text = styled('p', {
   margin: '0 0 2rem 0',
   padding: 0,
   textRendering: 'optimizeLegibility',
