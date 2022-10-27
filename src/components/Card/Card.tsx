f598f483b429623981957a2c462d7a3047dea0f3
Pablo Cingolani <cingopablo@gmail.com>
2022-10-27 13:38:03 +0200

    Updated Card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -3,8 +3,8 @@ import * as React from 'react'
 import { CardWrapper } from './Card.styles'
 import { CardProps } from './Card.types'
 
-export const Card: React.FC<CardProps> = ({ variant = 'default', children, css, onClick }) => (
-  <CardWrapper variant={variant} hover={!!onClick} css={css} onClick={onClick}>
+export const Card: React.FC<CardProps> = ({ variant = 'default', align = 'left', children, css, onClick }) => (
+  <CardWrapper variant={variant} align={align} hover={!!onClick} css={css} onClick={onClick}>
     {children}
   </CardWrapper>
 )
--- src/components/Card/Card.types.ts
+++ src/components/Card/Card.types.ts
@@ -3,9 +3,11 @@ import React from 'react'
 import { CSS } from '../../theming/stitches.config'
 
 export type CardVariant = 'default' | 'info' | 'success' | 'warning' | 'danger'
+export type CardAlign = 'left' | 'center' | 'right'
 
 export interface CardProps {
   variant?: CardVariant
+  align?: CardAlign
   children: React.ReactNode
   css?: CSS
   onClick?: () => void
