297c58c381b792032ca362e8e75735c129ad98f1
Pablo Cingolani <cingopablo@gmail.com>
2022-10-28 15:21:39 +0200

    Updated Card

--- src/components/ItemCard/ItemCard.styles.tsx
+++ src/components/ItemCard/ItemCard.styles.tsx
@@ -2,28 +2,38 @@ import { motion } from 'framer-motion'
 
 import { styled } from '../../theming/stitches.config'
 import { Grid } from '../Grid/Grid'
+import { Flex } from '../Flex/Flex'
+import { H3, Text } from '../Typography'
+
+const TitleMotion = motion(H3)
+const DescriptionMotion = motion(Text)
 
 export const ItemCardWrapper = styled(motion.div, {
-  padding: 'var(--spacing-4) 0',
+  border: '1px solid greenyellow',
+  padding: 'var(--spacing-7)',
   position: 'relative',
   width: '100%',
-})
-
-export const TitleWrapper = styled(motion.span, {
   display: 'flex',
   alignItems: 'center',
-  justifyContent: 'flex-start',
-  gap: 'var(--spacing-4)',
-})
+  justifyContent: 'space-between',
 
-export const IconWrapper = styled(motion.span, {
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-start',
-})
-
-export const ItemText = styled(Grid, {
-  '*': {
-    margin: '0 !important',
+  '@media (max-width: 600px)': {
+    display: 'grid',
   },
+})
+
+export const ImageWrapper = styled('div', {
+  display: 'none',
+  '@media (max-width: 600px)': {
+    display: 'block',
+  },
+})
+
+export const TitleWrapper = styled(TitleMotion, {
+  margin: '0 !important',
+})
+
+export const DescriptionWrapper = styled(DescriptionMotion, {
+  lineHeight: 'var(--font-height-heading)',
+  margin: '0 !important',
 })
--- src/components/ItemCard/ItemCard.tsx
+++ src/components/ItemCard/ItemCard.tsx
@@ -2,46 +2,42 @@ import React from 'react'
 
 import { Cursor } from '../Cursor/Cursor'
 import { Icon } from '../Icon/Icon'
-import { H4, Text } from '../Typography'
-import { IconWrapper, ItemCardWrapper, ItemText, TitleWrapper } from './ItemCard.styles'
+import { H1, H2, H3, H4, Text } from '../Typography'
+import { DescriptionWrapper, ImageWrapper, ItemCardWrapper, TitleWrapper } from './ItemCard.styles'
 import { ItemCardProps } from './ItemCard.types'
+import { Flex } from '../Flex/Flex'
 
 const titleVariants = {
   initial: {
-    x: -48,
-  },
-  hover: {
     x: 0,
-  },
-}
-
-const iconVariants = {
-  initial: {
-    opacity: 0,
-    x: -16,
-  },
-  hover: {
     opacity: 1,
-    x: 0,
+  },
+  hover: {
+    x: -40,
+    opacity: 0.5,
   },
 }
 
-export const ItemCard: React.FC<ItemCardProps> = ({ title, variant = 'primary', image, description, onClick, css }) => {
+const descriptionVariants = {
+  initial: {
+    x: 0,
+    opacity: 1,
+  },
+  hover: {
+    x: 40,
+    opacity: 0.5,
+  },
+}
+
+export const ItemCard = ({ title, variant = 'primary', image, description, onClick, css }: ItemCardProps) => {
   return (
     <Cursor content={image} variant={variant}>
       <ItemCardWrapper initial={'initial'} animate={'initial'} whileHover={'hover'} onClick={onClick} css={css}>
-        <ItemText gap={4}>
-          <TitleWrapper variants={titleVariants}>
-            <IconWrapper variants={iconVariants}>
-              <Icon.ArrowRight size={6} />
-            </IconWrapper>
-
-            <H4>{title}</H4>
-          </TitleWrapper>
-          <Text size={'medium'} weight={2}>
-            {description}
-          </Text>
-        </ItemText>
+        <ImageWrapper>{image}</ImageWrapper>
+        <TitleWrapper variants={titleVariants}>{title}</TitleWrapper>
+        <DescriptionWrapper size={'medium'} weight={2} variants={descriptionVariants}>
+          {description}
+        </DescriptionWrapper>
       </ItemCardWrapper>
     </Cursor>
   )
